2fd5e91da11c28fe6a3b141fd3a41e425c4116e2
Jay Mithani <114779988+jaymithani1998@users.noreply.github.com>
2023-02-23 13:44:43 -0500

    Minor code change in logic

--- network.tf
+++ network.tf
@@ -83,6 +83,15 @@ resource "aws_instance" "demo" {
   security_groups = ["${aws_security_group.instance.id}"]
   disable_api_termination     = false
   associate_public_ip_address = true
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 50
+    delete_on_termination = true
+  }
+
+  tags = {
+    "Name" = "demo ec2"
+  }
 }
 
 resource "aws_security_group" "instance" {
