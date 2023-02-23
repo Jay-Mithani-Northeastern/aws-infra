fd6b81ac6ab8f0d7086bae9555cff646f332e88f
Jay Mithani <114779988+jaymithani1998@users.noreply.github.com>
2023-02-23 02:32:59 -0500

    Changes to create ec2 instance

---
+++ instance_variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  default = "ami-00077dc30b012bfb8"
+}
+
+variable "key_name" {
+  default = "ec2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "profile" {
+  default = "dev"
+}
 variable "vpc_cidr_block" {
   type    = string
   default = "10.0.0.0/16"
@@ -12,4 +16,5 @@ locals {
   azs                 = data.aws_availability_zones.available.names
   public_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_cidr_blocks = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  ingress_port = [22, 80, 443, 5000]
 }
